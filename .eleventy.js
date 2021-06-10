0825664d124b61f527d9ec06b7d27e9b59973be5
Leandro Muzzupappa <lmuzzupappa@litebox.ai>
2021-06-10 11:15:07 -0300

    Se termina seccion 1 del home

--- .eleventy.js
+++ .eleventy.js
@@ -1,59 +1,61 @@
 const htmlmin = require('html-minifier');
 const fs = require('fs');
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setUseGitIgnore(false);
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setUseGitIgnore(false);
 
-  // Watch our compiled assets for changes
-  eleventyConfig.addWatchTarget('./src/compiled-assets/main.css');
-  eleventyConfig.addWatchTarget('./src/compiled-assets/main.js');
-  eleventyConfig.addWatchTarget('./src/compiled-assets/vendor.js');
+    // Watch our compiled assets for changes
+    eleventyConfig.addWatchTarget('./src/compiled-assets/main.css');
+    eleventyConfig.addWatchTarget('./src/compiled-assets/main.js');
+    eleventyConfig.addWatchTarget('./src/compiled-assets/vendor.js');
 
-  // Copy src/compiled-assets to /assets
-  eleventyConfig.addPassthroughCopy({ 'src/compiled-assets': 'assets' });
-  // Copy all images
-  eleventyConfig.addPassthroughCopy('src/assets/images');
-  eleventyConfig.addPassthroughCopy({ 'src/assets/meta': '/' });
+    // Copy src/compiled-assets to /assets
+    eleventyConfig.addPassthroughCopy({ 'src/compiled-assets': 'assets' });
+    // Copy all images
+    eleventyConfig.addPassthroughCopy('src/assets/images');
+    eleventyConfig.addPassthroughCopy({ 'src/assets/meta': '/' });
 
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, bs) {
-        bs.addMiddleware("*", (req, res) => {
-          const content_404 = fs.readFileSync('dist/404.html');
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
-
-  if (process.env.ELEVENTY_ENV === 'production') {
-    eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
-      if (outputPath.endsWith('.html')) {
-        const minified = htmlmin.minify(content, {
-          collapseInlineTagWhitespace: false,
-          collapseWhitespace: true,
-          removeComments: true,
-          sortClassName: true,
-          useShortDoctype: true,
-        });
-
-        return minified;
-      }
-
-      return content;
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, bs) {
+                bs.addMiddleware('*', (req, res) => {
+                    const content_404 = fs.readFileSync('dist/404.html');
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html; charset=UTF-8',
+                    });
+                    res.write(content_404);
+                    res.end();
+                });
+            },
+        },
     });
-  }
 
-  return {
-    dir: {
-      includes: '_components',
-      input: 'src',
-      layouts: '_layouts',
-      output: 'dist',
-    },
-    templateFormats: ['pug', 'md'],
-    htmlTemplateEngine: 'pug',
-  };
+    if (process.env.ELEVENTY_ENV === 'production') {
+        eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+            if (outputPath.endsWith('.html')) {
+                const minified = htmlmin.minify(content, {
+                    collapseInlineTagWhitespace: false,
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    sortClassName: true,
+                    useShortDoctype: true,
+                });
+
+                return minified;
+            }
+
+            return content;
+        });
+    }
+
+    return {
+        dir: {
+            includes: '_components',
+            input: 'src',
+            layouts: '_layouts',
+            output: 'dist',
+        },
+        templateFormats: ['pug', 'md'],
+        htmlTemplateEngine: 'pug',
+    };
 };
