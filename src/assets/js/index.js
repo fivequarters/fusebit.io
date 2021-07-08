16db00b419c845be47a24c60eff695df70ee7a4c
Leandro Muzzupappa <lmuzzupappa@litebox.ai>
2021-07-08 10:29:47 -0300

    Se modifica bolitas en la seccion features

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -16,134 +16,86 @@ if (sectionSupport) {
     const supportBase = new gsap.timeline();
 
     if (window.matchMedia('(min-width: 1175px)').matches) {
-        const supportCard1 = new gsap.timeline();
-        const supportCard1Reverse = new gsap.timeline();
-        const supportCard2 = new gsap.timeline();
-        const supportCard2Reverse = new gsap.timeline();
-        const supportCard3 = new gsap.timeline();
-        const supportCard3Reverse = new gsap.timeline();
+        function ballpath(
+            target,
+            targetPath,
+            cardPos,
+            offset,
+            duration,
+            indicatorName
+        ) {
+            function handleActiveCard(progress, val, currCard, debug) {
+                if (debug) console.log(progress);
 
-        function handleActiveCard(progress, val, currCard, debug) {
-            if (debug) console.log(progress);
-
-            if (progress == val) {
-                allCards[currCard].classList.add('card--active');
-            } else {
-                allCards[currCard].classList.remove('card--active');
+                if (progress == val) {
+                    allCards[currCard].classList.add('card--active');
+                } else {
+                    allCards[currCard].classList.remove('card--active');
+                }
             }
+
+            const gsapTimeline = new gsap.timeline();
+
+            const options = (id, isReverse) => ({
+                duration: 0.5,
+                motionPath: {
+                    path: `.support__path--${id}--path`,
+                    align: `.support__path--${id}--path`,
+                    alignOrigin: [0.5, 0.5],
+                    start: isReverse ? 1 : 0,
+                    end: isReverse ? 0 : 1,
+                },
+            });
+            gsapTimeline.to(target, options(targetPath, false));
+
+            // Path 1
+            new ScrollMagic.Scene({
+                triggerElement: '.support',
+                duration: duration,
+                offset: offset,
+                triggerHook: 0.75,
+            })
+                .setTween(gsapTimeline)
+                .on('progress', (e) => {
+                    if (targetPath !== 'base') {
+                        handleActiveCard(e.progress, 1, cardPos, false);
+                    }
+                })
+                //.addIndicators({ name: indicatorName })
+                .addTo(scrollMagicController);
         }
-
-        const supportCardOption = (id, isReverse) => ({
-            duration: 0.5,
-            motionPath: {
-                path: `.support__path--card${id}--path`,
-                align: `.support__path--card${id}--path`,
-                alignOrigin: [0.5, 0.5],
-                start: isReverse ? 1 : 0,
-                end: isReverse ? 0 : 1,
-            },
-        });
-
-        supportCard1.to(supportDot, supportCardOption(1, false));
-        supportCard1Reverse.to(supportDot, supportCardOption(1, true));
-        supportCard2.to(supportDot, supportCardOption(2, false));
-        supportCard2Reverse.to(supportDot, supportCardOption(2, true));
-        supportCard3.to(supportDot, supportCardOption(3, false));
-        supportCard3Reverse.to(supportDot, supportCardOption(3, true));
-
-        supportBase.to(supportDot, {
-            duration: 0.5,
-            motionPath: {
-                path: `.support__path--base--path`,
-                align: `.support__path--base--path`,
-                alignOrigin: [0.5, 0.5],
-            },
-        });
-
-        // Support Base pin
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            duration: 3900,
-            triggerHook: 0,
-            offset: window.matchMedia('(max-width: 1400px)').matches ? 80 : 0,
-        })
-            .setPin('.support')
-            .addTo(scrollMagicController);
-
-        // Support card 1
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            duration: 500,
-            triggerHook: 0,
-        })
-            .setTween(supportCard1)
-            .on('progress', (e) => handleActiveCard(e.progress, 1, 0, false))
-            .addTo(scrollMagicController);
-
-        // Support card 1 Reverse
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            offset: 900,
-            duration: 300,
-            triggerHook: 0,
-        })
-            .setTween(supportCard1Reverse)
-            .on('progress', (e) => handleActiveCard(e.progress, 0, 0, false))
-            .addTo(scrollMagicController);
-
-        // Support card 2
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            offset: 1300,
-            duration: 500,
-            triggerHook: 0,
-        })
-            .setTween(supportCard2)
-            .on('progress', (e) => handleActiveCard(e.progress, 1, 1, false))
-            .addTo(scrollMagicController);
-
-        // Support card 2 Reverse
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            offset: 2200,
-            duration: 300,
-            triggerHook: 0,
-        })
-            .setTween(supportCard2Reverse)
-            .on('progress', (e) => handleActiveCard(e.progress, 0, 1, false))
-            .addTo(scrollMagicController);
-
-        // Support card 3
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            offset: 2600,
-            duration: 500,
-            triggerHook: 0,
-        })
-            .setTween(supportCard3)
-            .on('progress', (e) => handleActiveCard(e.progress, 1, 2, false))
-            .addTo(scrollMagicController);
-
-        // Support card 3 Reverse
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            offset: 3500,
-            duration: 400,
-            triggerHook: 0,
-        })
-            .setTween(supportCard3Reverse)
-            .on('progress', (e) => handleActiveCard(e.progress, 0, 2, false))
-            .addTo(scrollMagicController);
-
-        // Support base path
-        new ScrollMagic.Scene({
-            triggerElement: '.support',
-            offset: 3900,
-            duration: 1000,
-            triggerHook: 0,
-        })
-            .setTween(supportBase)
-            .addTo(scrollMagicController);
+        ballpath(
+            '#support__dot--card0',
+            'base',
+            3,
+            300,
+            sectionSupport.getBoundingClientRect().height,
+            'support card base'
+        );
+        ballpath(
+            '#support__dot--card1',
+            'card1',
+            0,
+            300,
+            220,
+            'support card 1'
+        );
+        ballpath(
+            '#support__dot--card2',
+            'card2',
+            1,
+            300,
+            220,
+            'support card 2'
+        );
+        ballpath(
+            '#support__dot--card3',
+            'card3',
+            2,
+            300,
+            220,
+            'support card 3'
+        );
     } else {
         supportBase.to(supportDot, {
             duration: 0.5,
@@ -212,7 +164,7 @@ if (sectionFeatures) {
             triggerHook: 0.4,
         })
             .setTween(featuresBase)
-            .addIndicators({ name: 'Base path mobile' })
+            //.addIndicators({ name: 'Base path mobile' })
             .addTo(scrollMagicController);
     }
 
@@ -224,7 +176,7 @@ if (sectionFeatures) {
             triggerHook: 0.7,
         })
             .setClassToggle('#f' + i, 'features__list--item-active')
-            .addIndicators()
+            //.addIndicators()
             .addTo(scrollMagicController);
     }
 }
