b0f3acf0c5f2a5e5996e81b2b8ff6e64a9011352
gfaraone <gfaraone@litebox.ai>
2021-08-06 10:44:24 -0300

    Fix recall of images

--- src/_pages/_pages.11tydata.js
+++ src/_pages/_pages.11tydata.js
@@ -1,3 +1,3 @@
 module.exports = {
-    permalink: process.env.ELEVENTY_ENV === 'production' ? "| #{ page.filePathStem.replace('/_pages/', '/') }/index.html" : "| #{ page.filePathStem.replace('/_pages/', '/') }.html",
+    permalink: process.env.ELEVENTY_ENV === 'production' ? "| #{ page.filePathStem.replace('/_pages/', '/') + (page.filePathStem === '/_pages/index' ? '.html' : '/index.html') }" : "| #{ page.filePathStem.replace('/_pages/', '/') }.html",
 };
