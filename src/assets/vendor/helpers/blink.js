82021d7c046cdaf687f9fc0eb61580643e2b4f74
Federico Peralta <fperalta@litebox.ai>
2021-08-09 15:29:54 -0300

    finish investors

--- src/assets/vendor/helpers/blink.js
+++ src/assets/vendor/helpers/blink.js
@@ -1,6 +1,5 @@
 function blink(el, duration = 0.3, repeatDelay = 0) {
-    const tl = gsap.timeline({ repeat: -1, repeatDelay });
+    const tl = new TimelineMax({ repeat: -1, repeatDelay });
 
-    tl.to(el, { duration, alpha: 0 });
-    tl.to(el, { duration, alpha: 1 });
+    tl.to(el, duration, { alpha: 0 }).to(el, duration, { alpha: 1 });
 }
