2d3cc07b3b3a89a35e52ec6097c62c05ba9df181
Benn Bollay <bb.github@magitech.org>
2021-08-29 18:51:11 -0700

    Add robots.txt to test-website publish

---
+++ scripts/publish_robots_disallow.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# -- Standard Header --
+echoerr() { printf "%s\n" "$*" >&2; }
+export FUSEBIT_DEBUG=
+
+# -- Optional Parameters --
+AWS_PROFILE=${AWS_PROFILE:=default}
+
+# -- Script --
+set -e
+
+echo User-agent: * Disallow: / > ./robots.txt
+
+aws --profile=${AWS_PROFILE} s3 sync --acl public-read --cache-control max-age=300 \
+  ./robots.txt \
+  s3://${S3_BUCKET}
