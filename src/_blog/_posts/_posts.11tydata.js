5d68e26f23e0a58f312e675c9006363c1dd18749
Federico Peralta <88199805+fperaltalitebox@users.noreply.github.com>
2021-08-31 12:08:17 -0300

    [ENG 378] Add blog template (#51)
    * init blog template
    
    * add blog template
    
    * add post layout
    
    * add and parse post's frontmatter
    
    * add post list
    
    * test deploy
    
    * add different images
    
    * add code highlighting
    
    * improve code styles
    
    * add post card animations
    
    * add post img border radius
    
    * add pagination
    
    * add blog navigation
    
    * improve load more logic
    
    * add load more button styles
    
    * style inner navigation buttons
    
    * fix hero img overlapping with title
    
    * style hero info
    
    * add autor avatar, split tags
    
    * integrate tag list
    
    * add dynamic permalinks
    
    * add date parsing
    
    * parse date
    
    * format post date
    
    * add twilio, segment, github, auth0 post
    
    * add author avatar
    
    * add posts from production fusebit blog
    
    * add excerpts
    
    * restore about images
    
    * remove avatar component

---
+++ src/_blog/_posts/_posts.11tydata.js
@@ -0,0 +1,18 @@
+/* eslint-disable object-curly-newline */
+const { getYear, getDate, getMonth } = require('date-fns');
+
+module.exports = {
+    eleventyComputed: {
+        permalink: (data) => {
+            const date = new Date(data.date.replace('-', '/'));
+            const year = getYear(date);
+            const day = `${getDate(date)}`.padStart(2, '0');
+            const month = `${getMonth(date) + 1}`.padStart(2, '0');
+
+            // eslint-disable-next-line no-undef
+            const slug = data.post_slug ? data.post_slug : filters.slug(data.post_title || '');
+
+            return `blog/${year}/${month}/${day}/${slug}.html`;
+        },
+    },
+};
