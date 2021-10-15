a482545e4fef5bd800f0be97d3a14f135d7751e4
Federico Peralta <fperalta@litebox.ai>
2021-10-15 17:24:45 -0300

    implemente dynamic canonical and base url's

--- src/_data/env.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    canonicalUrl: process.env.BASE_URL,
-};
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 module.exports = {
     url: process.env.BASE_URL,
+    canonicalUrl: process.env.CANONICAL_URL,
 };
