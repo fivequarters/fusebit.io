343d76508f4070ddd3389cdf8fd78f65394ae470
Martin Fuster <mfuster@litebox.ai>
2021-10-21 13:16:07 -0300

    add scrollable modal

--- src/assets/vendor/helpers/openModal.js
+++ src/assets/vendor/helpers/openModal.js
@@ -2,6 +2,6 @@ function openModal(modalId) {
     const modal = document.querySelector(modalId);
     const overlay = document.querySelector('.modal__overlay');
 
-    modal.style.display = 'block';
-    overlay.style.display = 'block';
+    modal.style.display = 'flex';
+    overlay.style.display = 'flex';
 }
