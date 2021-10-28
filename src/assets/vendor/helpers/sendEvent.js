315844f7b8d125fbe218b7ef06d521b04ef3395a
Bruno Krebs <krebs.bruno@gmail.com>
2021-10-28 01:52:17 +0000

    gtag gone;

--- src/assets/vendor/helpers/sendEvent.js
+++ src/assets/vendor/helpers/sendEvent.js
@@ -2,7 +2,7 @@ function sendEvent(category, action, label) {
     // TODO: Access to proccess.env to check if the path has / or not at the end
     const _label = label ? label : window.location.pathname.split('/')[1] || '';
 
-    gtag('event', action, {
+    analytics.track(action, {
         event_category: category,
         event_label: '/' + _label,
     });
