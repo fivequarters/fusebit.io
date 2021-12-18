17e6e75d5272529b7e950d1045deceb2c4c6da64
Federico Peralta <fperalta@litebox.ai>
2021-12-17 21:57:22 -0300

    improve usability

--- src/assets/vendor/helpers/dropdown.js
+++ src/assets/vendor/helpers/dropdown.js
@@ -1,36 +1,59 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable func-names */
 function Dropdown(id, onChange) {
-    this.isOpen = true;
     this.root = document.getElementById(id);
     this.items = this.root.querySelectorAll('.dropdown-item');
+    this.menu = this.root.querySelector('.dropdown-menu');
+    this.open = false;
     this.init = function () {
+        document.addEventListener('click', (event) => {
+            const isClickInside = this.root.contains(event.target);
+
+            if (!isClickInside && this.open) {
+                this.toggle();
+            }
+        });
+
+        this.root.addEventListener('click', (event) => {
+            if (this.menu.contains(event.target)) {
+                return;
+            }
+            this.toggle();
+        });
+
         this.root.querySelector('span').innerHTML = `Category: ${
             this.root.querySelector('.dropdown-item').dataset.label
         }`;
 
         for (let i = 0; i < this.items.length; i++) {
             this.items[i].addEventListener('click', (e) => {
+                e.stopPropagation();
                 this.root.querySelector(
                     'span',
                 ).innerHTML = `Category: ${e.target.dataset.label}`;
+                this.toggle();
                 onChange?.(e.target.dataset.value);
             });
         }
 
         window.Popper.createPopper(
             this.root,
-            this.root.querySelector('.dropdown-menu'),
+            this.menu,
             {
                 placement: 'bottom-start',
-                strategy: 'fixed',
+                strategy: 'absolute',
+                modifiers: [
+                    {
+                        name: 'hide',
+                    },
+                ],
             },
         );
     };
 
     this.toggle = function () {
-        this.isOpen = !this.isOpen;
-        this.content.classList.toggle('dropdown-content--open');
+        this.open = !this.open;
+        this.menu.classList.toggle('dropdown-menu--open');
     };
 
     this.init();
