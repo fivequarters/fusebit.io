c76689a6df2c76f05728f4ba8e2e169d1347f199
Federico Peralta <fperalta@litebox.ai>
2021-12-18 19:42:03 -0300

    fix

--- src/_pages/_pages.11tydata.js
+++ src/_pages/_pages.11tydata.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 // eslint-disable-next-line func-names
 module.exports = async function () {
     const integrationsPromise = await fetch(
-        `${process.env.PORTAL_BASE_URL}feed/integrationsFeed.json`,
+        `${process.env.PORTAL_BASE_URL}/feed/integrationsFeed.json`,
     );
 
     const integrations = await integrationsPromise.json();
