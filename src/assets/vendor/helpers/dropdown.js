36f3d2e61c3a50a74289f6b372eef95b7f348bf8
Federico Peralta <fperalta@litebox.ai>
2021-12-17 21:01:14 -0300

    test popper

--- src/assets/vendor/helpers/dropdown.js
+++ src/assets/vendor/helpers/dropdown.js
@@ -5,16 +5,27 @@ function Dropdown(id, onChange) {
     this.root = document.getElementById(id);
     this.items = this.root.querySelectorAll('.dropdown-item');
     this.init = function () {
-        this.root.querySelector('span').innerHTML =
-            `Category: ${this.root.querySelector('.dropdown-item').dataset.label}`;
+        this.root.querySelector('span').innerHTML = `Category: ${
+            this.root.querySelector('.dropdown-item').dataset.label
+        }`;
 
         for (let i = 0; i < this.items.length; i++) {
             this.items[i].addEventListener('click', (e) => {
-                this.root.querySelector('span').innerHTML =
-                    `Category: ${e.target.dataset.label}`;
+                this.root.querySelector(
+                    'span',
+                ).innerHTML = `Category: ${e.target.dataset.label}`;
                 onChange?.(e.target.dataset.value);
             });
         }
+
+        window.Popper.createPopper(
+            this.root,
+            this.root.querySelector('.dropdown-menu'),
+            {
+                placement: 'bottom-start',
+                strategy: 'fixed',
+            },
+        );
     };
 
     this.toggle = function () {
