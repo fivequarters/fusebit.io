2a9512d2c6422e9d25a2f612f76a5501f7c058f9
Federico Peralta <88199805+fperaltalitebox@users.noreply.github.com>
2021-12-23 19:03:36 -0300

    Eng 1122 Add new integrations page (#171)
    * wip
    
    * wip
    
    * wip
    
    * add pagination
    
    * add cta
    
    * responsive
    
    * add filter
    
    * style
    
    * test popper
    
    * improve usability
    
    * add intercom cb
    
    * wip
    
    * :)
    
    * clean up
    
    * use env
    
    * fix
    
    * improve cdn
    
    * add analytics
    
    * add integrations link
    
    * change texts
    
    * fix scroll
    
    * add animations
    
    * fix animation
    
    * removed disabled color
    
    * fix font
    
    * fix link
    
    * hide private
    
    * fix button

--- src/_pages/_pages.11tydata.js
+++ src/_pages/_pages.11tydata.js
@@ -1,3 +1,30 @@
-module.exports = {
-    permalink: process.env.ELEVENTY_ENV === 'production' ? "| #{ page.filePathStem.replace('/_pages/', '/') + (page.filePathStem === '/_pages/index' ? '.html' : '/index.html') }" : "| #{ page.filePathStem.replace('/_pages/', '/') }.html",
+const fetch = require('node-fetch');
+
+// eslint-disable-next-line func-names
+module.exports = async function () {
+    const integrationsPromise = await fetch(
+        `${process.env.PORTAL_BASE_URL}/feed/integrationsFeed.json`,
+    );
+
+    const integrations = (await integrationsPromise.json()).filter((i) => !i.private);
+
+    return {
+        permalink:
+            process.env.ELEVENTY_ENV === 'production'
+                ? "| #{ page.filePathStem.replace('/_pages/', '/') + (page.filePathStem === '/_pages/index' ? '.html' : '/index.html') }"
+                : "| #{ page.filePathStem.replace('/_pages/', '/') }.html",
+        integrations: integrations.map((i) => ({
+            id: i.id,
+            logo: i.smallIcon,
+            name: i.name,
+            outOfPlan: i.outOfPlan,
+            catalog: i.tags.catalog,
+        })),
+        integrationCategories: [
+            'All',
+            ...new Set(
+                integrations.map((i) => i.tags.catalog.split(',')).flat(),
+            ),
+        ],
+    };
 };
---
+++ src/assets/vendor/helpers/dropdown.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable func-names */
+function Dropdown(id, onChange) {
+    this.root = document.getElementById(id);
+    this.items = this.root.querySelectorAll('.dropdown-item');
+    this.menu = this.root.querySelector('.dropdown-menu');
+    this.open = false;
+    this.init = function () {
+        document.addEventListener('click', (event) => {
+            const isClickInside = this.root.contains(event.target);
+
+            if (!isClickInside && this.open) {
+                this.toggle();
+            }
+        });
+
+        this.root.addEventListener('click', (event) => {
+            if (this.menu.contains(event.target)) {
+                return;
+            }
+            this.toggle();
+        });
+
+        for (let i = 0; i < this.items.length; i++) {
+            this.items[i].addEventListener('click', (e) => {
+                e.stopPropagation();
+                this.root.querySelector(
+                    '.dropdown-value',
+                ).innerHTML = e.target.dataset.label;
+                this.toggle();
+                onChange?.(e.target.dataset.value);
+            });
+        }
+
+        window.Popper.createPopper(this.root, this.menu, {
+            placement: 'bottom-start',
+            strategy: 'absolute',
+            modifiers: [
+                {
+                    name: 'hide',
+                },
+            ],
+        });
+    };
+
+    this.toggle = function () {
+        this.open = !this.open;
+        this.menu.classList.toggle('dropdown-menu--open');
+        this.root
+            .querySelector('.dropdown-arrow')
+            .classList.toggle('dropdown-arrow--open');
+    };
+}
