9aa310babf9e0764657f0e6857e5e667d7c239f8
Federico Peralta <fperalta@litebox.ai>
2021-12-24 20:23:02 -0300

    improve handleShowMore api

--- src/assets/vendor/helpers/handleShowMore.js
+++ src/assets/vendor/helpers/handleShowMore.js
@@ -1,25 +1,31 @@
-function handleShowMore(amount, elements, showMoreId, hiddenClass, force) {
+function handleShowMore({
+    amount, elements, showMoreId, hiddenClass, force,
+}) {
+    const shouldShowMore = () => {
+        if (document.querySelectorAll(elements).length > 0) {
+            document.getElementById(showMoreId).style.display = 'block';
+        } else {
+            document.getElementById(showMoreId).style.display = 'none';
+        }
+    };
     const renderMore = () => {
         [...document.querySelectorAll(elements)]
             .slice(0, amount)
             .forEach((el) => {
-                el.classList.remove(hiddenClass);
-
-                if (document.querySelectorAll(elements).length === 0) {
-                    document.getElementById(showMoreId).style.display = 'none';
-                }
+                el.classList.remove('show-more-item--hidden', 'show-more-item--filtered-out');
+                setTimeout(() => {
+                    el.classList.remove('show-more-item--hidden-transition');
+                }, 0);
             });
+        shouldShowMore();
     };
     const button = document.getElementById(showMoreId);
     const newButton = button.cloneNode(true);
     button.parentNode.replaceChild(newButton, button);
     newButton.addEventListener('click', renderMore);
 
-    if (document.getElementsByClassName(hiddenClass).length === 0) {
-        document.getElementById(showMoreId).style.display = 'none';
-    }
-
     if (force) {
         renderMore();
+        shouldShowMore();
     }
 }
