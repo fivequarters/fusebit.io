936ba14a13d8e2ee0784d0900d90e80e255350c1
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2022-01-27 18:19:28 -0300

    ENG 1285 Improvements to snippet make landing (#205)
    * add bases
    
    * map connectors
    
    * pull feed on the client
    
    * complete ui
    
    * add error message
    
    * load title data
    
    * load snippet descriptions
    
    * cleanup functions
    
    * add loader
    
    * add create button onclick and small cleanup
    
    * add variables
    
    * remove unnecessary vendor
    
    * add query not found error
    
    * fix
    
    * add pageview event
    
    * adjust analytics
    
    * feedback fixes
    
    Co-authored-by: Federico Peralta <fperalta@litebox.ai>

--- src/_data/site.js
+++ src/_data/site.js
@@ -7,4 +7,5 @@ module.exports = {
     url: process.env.BASE_URL,
     canonicalUrl: process.env.CANONICAL_URL,
     portalBaseUrl: process.env.PORTAL_BASE_URL,
+    connectorsFeedUrl: process.env.CONNECTORS_FEED_URL,
 };
