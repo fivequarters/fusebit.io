8dc9e2458c921903356ed0601ff9ae8aa183eab9
Martin Fuster <mfuster@litebox.ai>
2022-06-06 14:20:53 -0300

    create sidebar partial and adjust styles

--- src/assets/vendor/helpers/tableOfContents.js
+++ src/assets/vendor/helpers/tableOfContents.js
@@ -1,24 +1,22 @@
 function tableOfContents(childrenSelector) {
     const children = document.querySelector(childrenSelector)?.childNodes;
-    const sidebar = document.querySelector('.post__sidebar');
-    const sidebarCross = document.querySelector('.post__sidebar--cross-mobile');
-    const sidebarShadow = document.querySelector(
-        '.post__sidebar--mobile-shadow'
-    );
-    const sidebarSections = document.querySelector('.post__sidebar--sections');
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarCross = document.querySelector('.sidebar--cross-mobile');
+    const sidebarShadow = document.querySelector('.sidebar--mobile-shadow');
+    const sidebarSections = document.querySelector('.sidebar--sections');
     const relatedContent = document.getElementById('related-content');
     const relatedPostsCta = document.getElementById('related-content-cta');
     children.forEach((child) => {
         if (child?.nodeName?.includes('H2')) {
             const section = document.createElement('div');
             section.innerText = child.innerText;
-            section.classList.add('post__sidebar--section');
+            section.classList.add('sidebar--section');
             if (sidebarSections.childElementCount < 1) {
-                section.classList.add('post__sidebar--section-active');
+                section.classList.add('sidebar--section-active');
             }
             section.addEventListener('click', () => {
                 child.scrollIntoView();
-                sidebar.classList.remove('post__sidebar--mobile-active');
+                sidebar.classList.remove('sidebar--mobile-active');
             });
             sidebarSections.append(section);
         }
@@ -27,7 +25,7 @@ function tableOfContents(childrenSelector) {
     if (relatedContent && relatedPostsCta) {
         relatedPostsCta.addEventListener('click', () => {
             relatedContent.scrollIntoView();
-            sidebar.classList.remove('post__sidebar--mobile-active');
+            sidebar.classList.remove('sidebar--mobile-active');
         });
     }
 
@@ -41,17 +39,17 @@ function tableOfContents(childrenSelector) {
                         (section) => section.innerText === child.innerText
                     );
                     const currentSelectedSection = document.querySelector(
-                        '.post__sidebar--section-active'
+                        '.sidebar--section-active'
                     );
                     if (
                         newSectionToSelect &&
                         currentSelectedSection !== newSectionToSelect
                     ) {
                         currentSelectedSection?.classList?.remove?.(
-                            'post__sidebar--section-active'
+                            'sidebar--section-active'
                         );
                         newSectionToSelect?.classList?.add?.(
-                            'post__sidebar--section-active'
+                            'sidebar--section-active'
                         );
                     }
                 }
@@ -61,10 +59,10 @@ function tableOfContents(childrenSelector) {
     );
 
     sidebarShadow.addEventListener('click', (e) => {
-        sidebar.classList.remove('post__sidebar--mobile-active');
+        sidebar.classList.remove('sidebar--mobile-active');
     });
 
     sidebarCross.addEventListener('click', () => {
-        sidebar.classList.remove('post__sidebar--mobile-active');
+        sidebar.classList.remove('sidebar--mobile-active');
     });
 }
