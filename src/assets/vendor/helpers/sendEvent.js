82147659410ebf14845d3e3bb73242560a353709
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2022-06-16 19:50:27 -0300

    add linkedin track events (#355)

--- src/assets/vendor/helpers/sendEvent.js
+++ src/assets/vendor/helpers/sendEvent.js
@@ -1,12 +1,20 @@
 (() => {
-    const links = document.getElementsByClassName('track-with-segment');
-    links.forEach((link) => {
+    const analyticsLinks = document.getElementsByClassName('track-with-segment');
+    const linkedinLinks = document.getElementsByClassName('track-with-linkedin');
+    
+    analyticsLinks.forEach((link) => {
         const { eventName, objectLocation, ...rest } = link.dataset;
 
         analytics.trackLink(link, eventName, {
             objectLocation,
             domain: 'fusebit.io',
             ...(rest || {}),
+        });
+    });
+
+    linkedinLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            window.lintrk('track', { conversion_id: 8344146 });
         });
     });
 })();
