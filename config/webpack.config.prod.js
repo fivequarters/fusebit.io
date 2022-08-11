63b28d2a110b81411a6d80b5c7c98b843d89228c
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2022-08-11 13:56:26 -0300

    Mfuster/eng 1612 improve mobile performance (#391)
    * optimize
    
    * add usedExports

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,17 +1,22 @@
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 const common = require('./webpack.config.common.js');
 
 module.exports = merge(common, {
-  // Enable minification and tree-shaking
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new OptimizeCssAssetsPlugin({}),
-      new TerserPlugin({
-        extractComments: false,
-      }),
+    // Enable minification and tree-shaking
+    mode: 'production',
+    optimization: {
+        usedExports: true,
+        minimize: true,
+        minimizer: [new OptimizeCssAssetsPlugin({}), new TerserPlugin({})],
+    },
+    plugins: [
+        new CompressionPlugin({
+            algorithm: 'gzip',
+        }),
+        new BrotliPlugin(),
     ],
-  },
 });
