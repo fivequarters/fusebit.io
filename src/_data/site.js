40b3bf3b5127b454921a057f235b4971dfb4089d
Chris More <chris@chrismore.com>
2022-12-08 06:17:40 -0600

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    intercomAppId: process.env.INTERCOM_APP_ID,
     url: process.env.BASE_URL,
     canonicalUrl: process.env.CANONICAL_URL,
     portalBaseUrl: process.env.PORTAL_BASE_URL,
